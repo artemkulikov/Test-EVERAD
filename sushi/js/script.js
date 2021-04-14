0bb615ebe66c27375ee05715f27ce699757db5cd
Artem Kulikov <78357375+artemkulikov@users.noreply.github.com>
2021-04-14 23:05:23 +0800

    Add files via upload

---
+++ sushi/js/script.js
@@ -0,0 +1,45 @@
+function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    }
+    else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
+
+$('.slider__wrapper').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+           
+        }
+      }
+    ]
+  });
+
+var dateMy = new Date();
+var yar = dateMy.getFullYear();
+
+document.getElementById('year').innerHTML = yar;
