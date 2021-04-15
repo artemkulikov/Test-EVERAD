c82b0b8cb17f061eee686f2aa6eaa079828dec22
Artem Kulikov <78357375+artemkulikov@users.noreply.github.com>
2021-04-15 19:09:35 +0800

    Update script.js

--- sushi/js/script.js
+++ sushi/js/script.js
@@ -18,25 +18,6 @@ $('.slider__wrapper').slick({
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-           
-        }
-      }
-    ]
   });
 
 var dateMy = new Date();
