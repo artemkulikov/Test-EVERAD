2bab2b2f6862f48887c4ca2c08d880567a92e083
Artem Kulikov <78357375+artemkulikov@users.noreply.github.com>
2021-04-21 11:27:04 +0300

    Add files via upload

--- sushi/js/script.js
+++ sushi/js/script.js
@@ -14,11 +14,39 @@ testWebP(function (support) {
     }
 });
 
-$('.slider__wrapper').slick({
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  });
+
+let owl = $(".owl-carousel");
+owl.owlCarousel({
+    loop: true
+    , items: 1
+    , margin: 50
+    , dotsContainer: ".dots"
+    , dotsEach: true
+    , responsive: {
+        0: {
+            items: 1
+        , }
+        , 700: {
+            items: 1
+        , }
+        , 1000: {
+            margin: 300
+            , items: 1
+        , }
+    , }
+, });
+
+$(".arrow-right").click(function () {
+    owl.trigger("next.owl.carousel");
+});
+
+$(".arrow-left").click(function () {
+
+    owl.trigger("prev.owl.carousel", [300]);
+});
+$(".dot").click(function () {
+    owl.trigger("to.owl.carousel", [$(this).index(), 300]);
+});
 
 var dateMy = new Date();
 var yar = dateMy.getFullYear();
